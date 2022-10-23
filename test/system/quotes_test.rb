6ebcfee09018c87e50b8671fa45782a492e6fef8
sudobooo <marsdoghelena@gmail.com>
2022-10-24 00:44:24 +0400

    Add turbo frames and streams for project

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,24 +1,8 @@
-# frozen_string_literal: true
-
 require 'application_system_test_case'
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first)
-  end
-
-  test 'Creating a new quote' do
-    visit quotes_path
-    assert_selector 'h1', text: 'Quotes'
-
-    click_on 'New quote'
-    assert_selector 'h1', text: 'New quote'
-
-    fill_in 'Name', with: 'Capybara quote'
-    click_on 'Create quote'
-
-    assert_selector 'h1', text: 'Quotes'
-    assert_text 'Capybara quote'
+    @quote = Quote.ordered.first
   end
 
   test 'Showing a quote' do
@@ -28,14 +12,28 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector 'h1', text: @quote.name
   end
 
+  test 'Creating a new quote' do
+    visit quotes_path
+    assert_selector 'h1', text: 'Quotes'
+
+    click_on 'New quote'
+    fill_in 'Name', with: 'Capybara quote'
+
+    assert_selector 'h1', text: 'Quotes'
+    click_on 'Create quote'
+
+    assert_selector 'h1', text: 'Quotes'
+    assert_text 'Capybara quote'
+  end
+
   test 'Updating a quote' do
     visit quotes_path
     assert_selector 'h1', text: 'Quotes'
 
     click_on 'Edit', match: :first
-    assert_selector 'h1', text: 'Edit quote'
-
     fill_in 'Name', with: 'Updated quote'
+
+    assert_selector 'h1', text: 'Quotes'
     click_on 'Update quote'
 
     assert_selector 'h1', text: 'Quotes'
